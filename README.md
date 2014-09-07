152097d1decf19824de24ce2e754ac19ffc4d787
kaiqigong <kaiqigong@cloud3edu.com>
2014-09-07 14:31:08 +0800

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-dahuoApp
-========
+Ionic-Coffee-Scss App Base
+=====================
 
-mobile app for dahuo
+Go to [Additional Stuff](#additional-stuff) for quick start  
+A starting project for Ionic that optionally supports using custom SCSS.  
+The scaffold is made from [ionicframework](http://ionicframework.com/getting-started/)  
+And add some refinements.   
+
+## Additional stuff
++ Added coffee and scss support  
++ Auto inject bower js,css to index.html  
++ Auto inject compiled js/css to index.html
++ Auto inject custom scss to scss/app.scss
+
+## Pre-requests
+```sudo npm install -g cordova ionic```  
+```sudo npm install -g gulp```  
+```cordova platform add ios```  
+```sudo npm install -g ios-sim```
+
+## Usage
++ Clone this repo  
+```
+cd proDir
+npm install
+gulp install
+npm start // will compile coffee, scss, inject, watch and start server at :8100
+ionic build ios // build ios
+ionic emulate ios // emulate
+```
++ The gulp use liveload, install this [plugin](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei) for chrome. 
+
+## Contribution
++ Feel free to make pull requests!
---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp = require('gulp'),
+gutil = require('gulp-util'),
+bower = require('bower'),
+concat = require('gulp-concat'),
+sass = require('gulp-sass'),
+minifyCss = require('gulp-minify-css'),
+rename = require('gulp-rename'),
+sh = require('shelljs'),
+coffee = require('gulp-coffee'),
+inject = require("gulp-inject"),
+angularFilesort = require('gulp-angular-filesort'),
+bowerFiles = require('main-bower-files'),
+livereload = require('gulp-livereload'),
+base = {
+  www: './www/'
+},
+paths = {
+  sass: ['./www/scss/**/*.scss'],
+  coffee: ['./www/coffee/**/*.coffee'],
+  index: './www/index.html',
+},
+// you can change including files by editing the bower.json
+bowerPath = bowerFiles({
+  paths:{
+    bowerDirectory: './www/lib'
+  }
+});
+gulp.task('default', ['sass', 'coffee', 'index', 'watch']);
+
+gulp.task('index', function () {
+  console.log(bowerPath);
+  gulp.src(paths.index)
+  .pipe(inject(
+    gulp.src(['./www/js/**/*.js','./www/css/**/*.css'], {read: false}).pipe(angularFilesort())
+    , {relative: true, ignorePath: '/www/', addRootSlash: false}
+    ))
+  .pipe(inject(
+    gulp.src(bowerPath)
+    , {name: 'bower', relative: true, ignorePath: '/www/', addRootSlash: false}
+    ))
+  .pipe(gulp.dest('./www'));
+});
+
+gulp.task('sass', function(done) {
+  gulp.src('./www/scss/app.scss')
+  .pipe(inject(gulp.src(['./www/scss/**/*.scss','!./www/scss/app.scss'], {read: false})
+    ,{starttag: '// inject:scss',
+    endtag: '// endinject',
+    transform: function (filepath) {
+      console.log(filepath);
+      return '@import "' + filepath + '";';
+    },relative: true}))
+  .pipe(gulp.dest('./www/scss/'))
+  .pipe(sass())
+  .pipe(gulp.dest('./www/css/'))
+  // minify
+  // .pipe(minifyCss({
+  //   keepSpecialComments: 0
+  // }))
+  // .pipe(rename({ extname: '.min.css' }))
+  // .pipe(gulp.dest('./www/css/'))
+  .on('end', done);
+});
+
+gulp.task('coffee', function(done) {
+  gulp.src(paths.coffee)
+  .pipe(coffee({bare: true}))
+  .pipe(gulp.dest('./www/js/'))
+  .on('end', done);
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.coffee, ['coffee']);
+  gulp.watch([paths.index, 'bower.json'],['index']);
+  gulp.watch('./www/css/**/*.css').on('change', livereload.changed);
+});
+
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+  .on('log', function(data) {
+    gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+  });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+      );
+    process.exit(1);
+  }
+  done();
+});
